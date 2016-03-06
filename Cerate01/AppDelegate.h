774758c7b612bf26d8cdd9b03a181098b1de0a76
ChingPingTseng <davidtseng200@gmail.com>
2016-03-06 20:23:08 +0800

    Initial Commit

---
+++ Cerate01/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Cerate01
+//
+//  Created by ChingPing Tseng on 3/6/16.
+//  Copyright © 2016 ChingPingTseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Cerate01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cerate01
+//
+//  Created by ChingPing Tseng on 3/6/16.
+//  Copyright © 2016 ChingPingTseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
